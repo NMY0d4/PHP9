3e7118895dc48b5c49d918dc0fc1d4e76a0c0159
Grégory MARINI <greg.marini@hotmail.fr>
2022-02-18 16:23:14 +0100

    modif recu sur vs

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require('inc/function.php');
  $fruits = [
     "banane" => '-- Banane 🍌 --',
     "raisins" => '-- Raisins 🍇 --',
-    "cerise" => '-- Cerise 🍒 Cherry--',
+    "cerise" => '-- Cerise 🍒 Cherry --',
     "fraise" => '-- Fraise 🍓 --',                
 ]; 
 
